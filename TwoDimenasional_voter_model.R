a51c82259e7006893a117f00b367e27fe9ebb766
Abel Camacho <cuatrooctavos@yahoo.com.mx>
2017-03-06 11:36:44 +0100

    Small changes

--- TwoDimenasional_voter_model.R
+++ TwoDimenasional_voter_model.R
@@ -1,14 +1,18 @@
+
+# ################################################
+# Load library
+# ################################################
 library(lattice)
 
 # ################################################
-# Parameters
+# Function
 # ################################################
 
 mayority_model_dim2<-function(chosen_time,N, condition){
-# t equals time
+
 # s equals magnetization
 s<-rep(0, chosen_time)
-
+# Time when system reaches an absorbing state 
 stoptime2<-0
 
 # Number of simulations
@@ -35,6 +39,7 @@ agents0[agents0[,2]<=2 | agents0[,3]<=2 | agents0[,2]>=N-2 |agents0[,3]-1>=N-2,1
 
 
 
+# Compute coordinates of agents
 x_axis<-rep(1,N)
 for(i in 2:N)
 { x_axis <-c(x_axis,rep(i,N))}
@@ -42,47 +47,49 @@ for(i in 2:N)
 y_axis <-seq(1,N,by=1)
 for(i in 2:N)
 { y_axis <-c(y_axis,seq(1,N,by=1))}
+# Agents cordinate are in column 2 and 3
 agents0[,2]<-x_axis
 agents0[,3]<-y_axis
 
 
-
-
  
-# create plot
+# Create plot:
+# Color black: opinon -1, color red: opinion 1
 colmat<-matrix(agents0[,1], nrow=N ,ncol=N)
 levelplot(colmat,col.regions=c('black','red'),xlab='',ylab='',colorkey=F, main='Initial Condition')
-#dev.off()
 
 
 
 # i is the time
-
+#  Simulations start at period 1
 i<-1
+# Simulation finished at period chosen_time
 while(i<=chosen_time){
-  # choose an agent at random
+  # Choose an agent at random
   x1<-sample(1:N,1)
   x2<-sample(1:N,1)
   
-  # neighbourhood of the agent
+  # Neighbourhood of the agent
   potential_choice<-agents0[abs(agents0[,2]-x1)+abs(agents0[,3]-x2)<=1 
                             & abs(agents0[,2]-x1)+abs(agents0[,3]-x2)>0,]
                             
 # Choose neighbour                            
   choice<-sample(1:nrow(potential_choice),1)
-  
+ # Probability that agent is influenced by its neighbour 
   p<-1/4*(1-agents0[agents0[,2]==x1 &agents0[,3]==x2,1]*potential_choice[choice,1])
   
   agents0[agents0[,2]==x1 &agents0[,3]==x2,1]<-sample(c(agents0[agents0[,2]==x1 &agents0[,3]==x2,1] ,potential_choice[choice,1]),1,prob=c(1-p,p))
   
-  # magnetization
+  # Magnetization
   s[i]<-sum(agents0[,1])/N^2
-  # if  |s|=1 stop loop.
+  # If  |s|=1 stop loop.
   # The system has reached an absorbing state.  
   if(s[i]==1 | s[i]==-1){
     stoptime2<-i
     i<-t
+    # Print when state reaches equilibrium
     print(stoptime2)
+    # Stop simulations
     break
   }
   # plot only certain steps   
@@ -90,7 +97,6 @@ while(i<=chosen_time){
   	colmat<-matrix(agents0[,1], nrow=N ,ncol=N)
   	print(levelplot(colmat,col.regions=c('black','red'),ylab='',colorkey=F ,main=list(paste0('Period: ',i),side=1,line=0.5),xlab=''))
  
-  
   }
   i<-i+1
 }
